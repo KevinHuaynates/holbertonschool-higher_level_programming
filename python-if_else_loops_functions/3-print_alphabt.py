9147f1ca87f49d483d11ac86ba3d2649a622fc13
KevinHuaynates <6992@holbertonstudents.com>
2023-11-12 20:04:21 -0800

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-print("".join(chr(i) for i in range(97, 123) if chr(i) not in ['q', 'e']))
+
+for i in range(ord('a'), ord('z') + 1):
+        if chr(i) not in 'eq':
+                    print("{}".format(chr(i)), end="")
+                    print()
