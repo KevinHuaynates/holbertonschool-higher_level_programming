c362c2838c0e51fe034808595981fe636fbaf88e
KevinHuaynates <6992@holbertonstudents.com>
2023-11-12 20:08:29 -0800

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-for i in range(ord('a'), ord('z') + 1):
-        if chr(i) not in 'eq':
-                    print("{}".format(chr(i)), end="")
-                    print()
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{:c}".format(i), end="")
