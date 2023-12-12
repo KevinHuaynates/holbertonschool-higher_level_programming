05fc84eea33444ea82ca603809a5d9426301fa51
KevinHuaynates <6992@holbertonstudents.com>
2023-12-11 22:06:47 -0800

    7-islower.py

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -5,6 +5,7 @@ def islower(c):
 if __name__ == "__main__":
     islower = __import__('7-islower').islower
 
+
     print("a is {}".format("lower" if islower("a") else "upper"))
     print("H is {}".format("lower" if islower("H") else "upper"))
     print("A is {}".format("lower" if islower("A") else "upper"))
