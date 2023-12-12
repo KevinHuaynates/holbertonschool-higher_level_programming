d5d6040b805e1451c6445402d4e9de5cfa08625e
KevinHuaynates <6992@holbertonstudents.com>
2023-12-11 23:05:59 -0800

    calculator_1.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,16 +1,10 @@
-#!/usr/bin/python
-a = 10
-b = 5
-
+#!/usr/bin/python3
 if __name__ == "__main__":
-
     from calculator_1 import add, sub, mul, div
-    result_add = add(a, b)
-    result_sub = sub(a, b)
-    result_mul = mul(a, b)
-    result_div = div(a, b)
+    a = 10
+    b = 5
 
-    print("{:d} + {:d} = {:d}".format(a, b, result_add))
-    print("{:d} - {:d} = {:d}".format(a, b, result_sub))
-    print("{:d} * {:d} = {:d}".format(a, b, result_mul))
-    print("{:d} / {:d} = {:d}".format(a, b, result_div))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
