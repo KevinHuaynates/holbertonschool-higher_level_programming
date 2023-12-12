94a475d90a85a1f9e091f9d6a1e7d716a21c940f
KevinHuaynates <6992@holbertonstudents.com>
2023-12-11 23:49:20 -0800

    7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a1, a2 = tuple_a + (0, 0)
-    b1, b2 = tuple_b + (0, 0)
-    return (a1 + b1, a2 + b2)
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    result = (a[0] + b[0], a[1] + b[1])
+    return result
+
 if __name__ == "__main__":
-    add_tuple = __import__('7-add_tuple').add_tuple
     tuple_a = (1, 89)
     tuple_b = (88, 11)
     new_tuple = add_tuple(tuple_a, tuple_b)
