2988578a5a538077e21d00db24cfbd95b14eebe2
KevinHuaynates <6992@holbertonstudents.com>
2023-12-13 21:44:31 -0800

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    Esta es una clase Square que define un cuadrado.
+    Attributes:
+    __size (int): El tamaño del cuadrado.
+    
+    """
+    def __init__(self, size):
+        self.__size = size
+
