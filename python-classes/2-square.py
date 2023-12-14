5e31f324dc2794868fd9852e647ee94bf32337c4
KevinHuaynates <6992@holbertonstudents.com>
2023-12-14 00:01:48 -0800

    2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Documentation Module - This module contains the definition of the Square class.
+"""
+
+
+class Square:
+    """
+    Square Class - Represents a square.
+    """
+    def __init__(self, size=0):
+        """
+        __init__ Method - Initializes a new instance of the Square class.
+        """
