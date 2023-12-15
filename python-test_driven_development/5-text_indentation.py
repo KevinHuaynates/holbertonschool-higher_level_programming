b1c7b5aad94d4559bc09de1c60f2adf3e0cd85f1
KevinHuaynates <6992@holbertonstudents.com>
2023-12-15 01:12:51 -0800

    5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -8,12 +8,14 @@ def text_indentation(text):
     """Function: atext_indentation(text)"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+
     buffer = " "
     for char in text:
         buffer += char
         if char in ['.', '?', ':']:
             print(buffer.strip()+" ")
             buffer = " "
+
     if buffer:
         print(buffer.strip())
 
