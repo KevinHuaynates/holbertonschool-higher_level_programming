89f701d5021b327a69bc6b6e53041fbed93e555e
KevinHuaynates <6992@holbertonstudents.com>
2023-12-15 00:36:38 -0800

    5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module contains a function to print a text with 2 new lines after each '.', '?', and ':'
+This module contains a function to print
+a text with 2 new lines after each '.', '?', and ':'
 """
 
 
@@ -8,6 +9,7 @@ def text_indentation(text):
     """
     Print a text with 2 new lines after each '.', '?', and ':'
     """
+
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
