a7f06cdd2ae5579f78daf25d4e6d68fcdcc0c512
KevinHuaynates <6992@holbertonstudents.com>
2023-12-17 14:23:44 -0800

    3-rectangle.py

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -55,5 +55,4 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         else:
-            return
-        "\n".join(["#" * self.__width for _ in range(self.__height)])
+            return "\n".join(["#" * self.__width for _ in range(self.__height)])
