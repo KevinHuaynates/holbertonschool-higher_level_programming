f03f5d74b2b5f12421a8d324c292cc57ad7a2124
KevinHuaynates <6992@holbertonstudents.com>
2023-12-17 15:14:27 -0800

    6-rectangle.py

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -41,17 +41,17 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
-    
-    def area(self):
-       """Method that returns the rectangle area"""
-       return self.__width * self.__height
 
-   def perimeter(self):
-       """Method that returns the rectangle perimeter"""
-       if self.__width == 0 or self.__height == 0:
-           return 0
-       else:
-           return 2 * (self.__width + self.__height)
+    def area(self):
+        """Method that returns the rectangle area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Method that returns the rectangle perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
 
     def __str__(self):
         """Method that returns a string representation of the rectangle"""
