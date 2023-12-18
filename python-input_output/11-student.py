0c2643d215f2962742c56838a05aa4c8c3626f13
KevinHuaynates <6992@holbertonstudents.com>
2023-12-17 18:48:53 -0800

    11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module with Student class."""
+
+
+class Student:
+    """Defines a student."""
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance."""
+        if attrs is None:
+            return self.__dict__
+        return {key: value for key,
+                value in self.__dict__.items() if key in attrs}
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance."""
+        self.__dict__.update(json)
