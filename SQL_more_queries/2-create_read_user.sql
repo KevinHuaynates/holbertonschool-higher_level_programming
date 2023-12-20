ffc8b848b00c9d7a6fb70368b345834f131f42d2
KevinHuaynates <6992@holbertonstudents.com>
2023-12-19 22:34:46 -0800

    2-create_read_user.sql

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+--Crea la base de datos hbtn_0d_2 y el usuario user_0d_2 con privilegio SELECT y contraseña user_0d_2_pwd
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
