ffa5d34eac4b30e140cf6e9ebd597d936eeefb27
KevinHuaynates <6992@holbertonstudents.com>
2023-12-19 23:01:44 -0800

    10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
--- Lista todos los programas contenidos en hbtn_0d_tvshows que tienen al menos un género vinculado.
+-- el nombre de la base de datos se pasará como un argumento del comando mysql
+
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
-
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
