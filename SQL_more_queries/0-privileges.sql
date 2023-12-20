0ebe07a310392edd97dd4c12bf83801586ada83b
KevinHuaynates <6992@holbertonstudents.com>
2023-12-19 22:27:38 -0800

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,16 +1,3 @@
---Lis all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-CREATE USER 'user_0d_1'@'localhost';
-
--- Otorgar privilegios a 'user_0d_1'
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Crear el usuario 'user_0d_2'
-CREATE USER 'user_0d_2'@'localhost';
-
--- Otorgar privilegios a 'user_0d_2'
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
--- Mostrar privilegios de 'user_0d_1' y 'user_0d_2'
+-- MySQL script to list all privileges of the MySQL users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
-
