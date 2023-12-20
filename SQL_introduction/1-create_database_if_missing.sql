243919a6ee91cf98a1ff1f7293c865abc6bcc20a
KevinHuaynates <6992@holbertonstudents.com>
2023-12-19 21:43:57 -0800

    9-full_creation.sql

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- Crear e database hbtn_0c_ si no existe
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Add a new column to the table first_table
+ALTER TABLE first_table
+ADD COLUMN new_column_name VARCHAR(256);
