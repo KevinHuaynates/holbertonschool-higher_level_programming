b37b6aad40dd1c4094f4884d9c923a27db23475d
KevinHuaynates <6992@holbertonstudents.com>
2023-12-20 21:57:51 -0800

    5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
-"""
-Script to list all cities of a specific state from the database hbtn_0e_4_usa
-"""
+"""Script to list all cities of a specific state from the database hbtn_0e_4_usa"""
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    username, password, database, state_name = sys.argv[1:]
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
 
     cursor = db.cursor()
-        cursor.execute("""
-        SELECT cities.name
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        WHERE states.name=%s
-        ORDER BY cities.id ASC
-        """, (state_name,))
 
-    rows = cursor.fetchall()
-    print(", ".join(city[0] for city in rows))
+    try:
+        query = "SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = %s) ORDER BY id ASC"
+        cursor.execute(query, (state_name,))
 
-    cursor.close()
-    db.close()
+        rows = cursor.fetchall()
+        result = ', '.join([row[1] for row in rows])
+        print(result)
 
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+
+    finally:
+        cursor.close()
+        db.close()
