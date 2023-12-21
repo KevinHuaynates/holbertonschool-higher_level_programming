9a6842f6cef21637cdf63345476022c5c1aa4424
KevinHuaynates <6992@holbertonstudents.com>
2023-12-20 21:12:17 -0800

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,36 +1,26 @@
 #!/usr/bin/python3
-
-"""Modulo: Script to filter states by user input"""
+"""
+Module:Script to filter states by user input
+"""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Check if all required arguments are provided
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
-        sys.exit(1)
 
-    # Get command line arguments
-    username, password, database, state_name = sys.argv[1:]
-    
-    # Connect to MySQL server
-    db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-    # Create a cursor object to interact with the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
     cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC", (state_name,))
 
-    # Prepare and execute SQL query
-    query = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
-    cursor.execute(query, (state_name,))
-
-    # Fetch all the rows
     rows = cursor.fetchall()
-    
-    # Display the results
     for row in rows:
         print(row)
 
-    # Close cursor and connection
     cursor.close()
     db.close()
