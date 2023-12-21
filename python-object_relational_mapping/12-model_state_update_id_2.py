3de38b47be885ac962dcc8cf2f19827ad9de5ac7
KevinHuaynates <6992@holbertonstudents.com>
2023-12-20 22:35:14 -0800

    12-model_state_update_id_2.py

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Script to change the name of a State object from the database hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, database = sys.argv[1:]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, database), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+
+    try:
+        state_to_update = session.query(State).filter_by(id=2).first()
+        state_to_update.name = "New Mexico"
+        session.commit()
+
+    except Exception as e:
+        print("Error:", e)
+
+    finally:
+        session.close()
