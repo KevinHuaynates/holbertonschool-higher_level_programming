ebb86168b8869f13030a8519327fe4b2dc8c2900
KevinHuaynates <6992@holbertonstudents.com>
2023-12-20 20:48:41 -0800

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,22 @@
-#!/usr/bin/python3
-"""Module to list all states from the database hbtn_0e_0_usa where name matches the argument"""
-
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC", (sys.argv[4],))
-    rows = cur.fetchall()
+    username, password, database, state_name = sys.argv[1:]
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+
+    query = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
+
+    cursor.execute(query, (state_name,))
+    rows = cursor.fetchall()
+
     for row in rows:
         print(row)
-    cur.close()
-    db.close()
 
+    cursor.close()
+    db.close()
