bf1bdcd044c95eef4b02053036dce03acabd1973
KevinHuaynates <6992@holbertonstudents.com>
2023-12-20 21:51:04 -0800

    5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -6,21 +6,24 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
-        sys.exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-    username, password, database, state_name = sys.argv[1:]
-    db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
     cursor = db.cursor()
-    query = """
-            SELECT GROUP_CONCAT(name ORDER BY cities.id ASC SEPARATOR ', ')
-            """
-    cursor.execute(query, (state_name,))
+        cursor.execute("""
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name=%s
+        ORDER BY cities.id ASC
+        """, (state_name,))
 
-    result = cursor.fetchone()[0]
-
-    print(result)
+    rows = cursor.fetchall()
+    print(", ".join(city[0] for city in rows))
 
     cursor.close()
     db.close()
