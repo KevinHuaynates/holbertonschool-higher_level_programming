26640c7ba371b5374f3863d4022c5ea95a6fcf6b
KevinHuaynates <6992@holbertonstudents.com>
2023-12-31 04:49:43 -0800

    4-print_hexa.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,14 +2,10 @@
 
 def uppercase(s):
     result = ""
-    count = 0
+    for char in s:
+        if ord('a') <= ord(char) <= ord('z'):
+            result += chr(ord(char) - ord('a') + ord('A'))
+        else:
+            result += char
 
-    if isinstance(s, str):
-        for char in s:
-            if ord('a') <= ord(char) <= ord('z'):
-                result += chr(ord(char) - ord('a') + ord('A'))
-            else:
-                result += char
-            count += 1
-
-    print("{}".format(result), end='\n' if count > 0 else '')
+    print("{}".format(result))
