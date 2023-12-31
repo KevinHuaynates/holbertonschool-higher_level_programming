65955478e149e9eb5218e1eca6086625be10610b
KevinHuaynates <6992@holbertonstudents.com>
2023-12-31 04:43:08 -0800

    4-print_hexa.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
 
+
 def uppercase(s):
     result = ""
+    count = 0
     for char in s:
         if ord('a') <= ord(char) <= ord('z'):
             result += chr(ord(char) - ord('a') + ord('A'))
         else:
             result += char
+        count += 1
 
-    print("{}".format(result))
+    print(result, end='\n' if count > 0 else '')
+
 
 # Test cases
-uppercase("best")
-uppercase("Best School 98 Battery street")
+uppercase("holberton")
+uppercase("Holberton School")
+uppercase("Holberton School, 98 battery street")
+uppercase("")
+uppercase(98)
+uppercase('z')
